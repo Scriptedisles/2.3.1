b57b12f5e0acf9f41dfe25e7d318216261417885
Scripted <cb443151@gmail.com>
2024-02-21 08:15:06 -0600

    Add files via upload

---
+++ decode copy.py
@@ -0,0 +1,91 @@
+#   decode.py 5-13-22
+#   Note this will not run in the code editor and must be downloaded
+from PIL import Image
+import platform
+
+im = Image.open("output.gif")
+rgb_im = im.convert('RGB')
+marker_coords = (0,0)
+detected = False
+DISTANCE_BETWEEN_BLOCKS = 0
+for i in range(0,rgb_im.height):
+  if detected == True:
+    break
+  for j in range(0,rgb_im.width):
+    r,g,b = rgb_im.getpixel((j,i))
+    if detected == True and b > 30:
+      break
+    if detected == True:
+      DISTANCE_BETWEEN_BLOCKS +=1
+    if b < 50 and r > 200 and detected == False: #find the first red pixel
+      marker_coords=(j,i)
+      detected = True
+DISTANCE_BETWEEN_BLOCKS += int(0.1*DISTANCE_BETWEEN_BLOCKS)
+print(marker_coords)
+print("Distance between blocks is: " , DISTANCE_BETWEEN_BLOCKS)
+# Changed MAX_CHARACTERS to 21
+MAX_CHARACTERS = 21
+BITS_IN_A_BYTE = 8
+MAX_BITS = MAX_CHARACTERS * BITS_IN_A_BYTE 
+
+my_array = []
+for letters in range(0,MAX_BITS):
+  my_array.append(0)
+
+UPPER_PIXEL_ROW = marker_coords[1] + DISTANCE_BETWEEN_BLOCKS
+LEFT_PIXEL_COL = marker_coords[0] + DISTANCE_BETWEEN_BLOCKS
+
+LOW_PIXEL_ROW = UPPER_PIXEL_ROW+(MAX_CHARACTERS*DISTANCE_BETWEEN_BLOCKS)
+RIGHT_PIXEL_COL = LEFT_PIXEL_COL+(BITS_IN_A_BYTE*DISTANCE_BETWEEN_BLOCKS)
+
+ # CHANGED POS VALUE TO 160
+pos=160
+for i in range(UPPER_PIXEL_ROW,LOW_PIXEL_ROW,DISTANCE_BETWEEN_BLOCKS):
+  for j in range(LEFT_PIXEL_COL,RIGHT_PIXEL_COL,DISTANCE_BETWEEN_BLOCKS):
+    r,g,b = rgb_im.getpixel((j,i))
+    if g < 254:
+      my_array[pos]=1
+      print(j,i)
+      # Changed Pos to minus
+    pos = pos - 1
+print(my_array)
+
+message_as_bits = ''
+for bit in my_array:
+  message_as_bits = message_as_bits + str(bit)
+  print("Current Bit" + str(bit))
+print(message_as_bits)
+
+
+letter = 0
+decoded = ''
+for n in range(0,MAX_BITS):
+  if n % 8 == 0:
+    if letter != 0:
+      decoded = decoded + chr(letter)
+      letter = 0
+    letter = int(message_as_bits[n]) * 128 + letter 
+  elif n % 8 == 1:
+    letter = int(message_as_bits[n]) * 64 + letter 
+  elif n % 8 == 2:
+    letter = int(message_as_bits[n]) * 32 + letter 
+  elif n % 8 == 3:
+    letter = int(message_as_bits[n]) * 16 + letter 
+  elif n % 8 == 4:
+    letter = int(message_as_bits[n]) * 8 + letter 
+  elif n % 8 == 5:
+    letter = int(message_as_bits[n]) * 4 + letter 
+  elif n % 8 == 6:
+    letter = int(message_as_bits[n]) * 2 + letter 
+  elif n % 8 == 7:
+    letter = int(message_as_bits[n]) * 1 + letter
+
+# the decoded variable is backwards, so we need to reverse it
+decoded = decoded[::-1]
+print(decoded)
+bbox = (LEFT_PIXEL_COL, UPPER_PIXEL_ROW, LEFT_PIXEL_COL+ BITS_IN_A_BYTE*DISTANCE_BETWEEN_BLOCKS, UPPER_PIXEL_ROW + MAX_CHARACTERS*DISTANCE_BETWEEN_BLOCKS)
+
+print(bbox)
+show_code_region = im.crop(box=bbox)
+show_code_region.save("code_region.gif")
+#show_code_region.show()
---
+++ encode.py
@@ -0,0 +1,84 @@
+#   encode.py 5-13-22
+#   Note this will not run in the code editor and must be downloaded
+# import the required modules
+import tkinter as tk
+import turtle as trtl
+from PIL import ImageGrab, Image, ImageDraw
+import mss
+import platform
+
+#store the message to a variable 
+message = "subscribe to Mr.V" # Change this to encode a different message. Length limit 20 characters.
+
+# Parse through the character and appened them to a list
+characters_as_ints = []
+for cha in message:
+  characters_as_ints.append(ord(cha))
+print(characters_as_ints)
+
+characters_as_bits = []
+for integ in characters_as_ints:
+  characters_as_bits.append('{0:08b}'.format(integ))
+print(characters_as_bits)
+
+bits_as_ints = []
+for index in range(0,len(characters_as_bits)):
+  for bit in characters_as_bits[index]:
+    bits_as_ints.append(bit)
+print(bits_as_ints)
+
+screen = trtl.getscreen()
+painter = trtl.Turtle()
+print(screen.window_width(), " x ", screen.window_height())
+painter.penup()
+painter.goto(-200,221)
+painter.shape("square")
+painter.goto(-221,221)
+painter.color("red")
+painter.stamp()
+painter.forward(21)
+painter.color("blue")
+
+message_length = len(bits_as_ints)
+index = 0
+while index < message_length:
+  if index % 8 == 0:
+    painter.goto(-200, painter.ycor()-21)
+  if bits_as_ints[index]=='1':
+    painter.stamp()
+  painter.forward(21)
+  index = index + 1
+
+screen = painter.getscreen()
+root = trtl.getcanvas().winfo_toplevel()
+
+def create_image(widget):
+  with mss.mss() as sct:
+    sct.shot(mon=1,output='fullscreen.gif') #mon is the monitor number of the primary display. Change if capturing incorrect display.
+    
+    x=root.winfo_rootx()
+    y=root.winfo_rooty()
+    
+    #Uncomment this section to get details about the upper left coordinate, height, and width of the window
+    """
+    width = widget.window_width()
+    height = widget.window_height()
+    print("width =", width, "height = ", height)
+    print("x is: ", x , " y is: ", y)
+    """
+    x1=x+widget.window_width()
+    y1=y+widget.window_height()
+
+    im = Image.open("fullscreen.gif")
+    bounds = x,y,x1,y1 
+    #Retina displays require a coefficient of 2 for pixel numbers
+    if platform.system() == 'Darwin': 
+      bounds = 2*x,2*y,2*x1,2*y1
+    print("Turtle bounding box: ", bounds)
+    im = im.crop(box=bounds)
+    im.save("output.gif")
+
+
+create_image(screen)
+
+#screen.mainloop()
